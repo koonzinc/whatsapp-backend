1d0e5719960810200a832b5ba92ed6ed9f199eee
koonzinc <koonz.eth@gmail.com>
2022-10-29 14:03:55 -0400

    update backend with cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import Messages from './dbMessages.js';
 import Pusher from 'pusher';
+import cors from 'cors';
 // app config
 const app = express();
 const port = process.env.PORT || 9000;
@@ -17,12 +18,7 @@ const pusher = new Pusher({
 
 // middleware 
 app.use(express.json());
-
-app.use((req,res,next)=> {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    next();
-});
+app.use(cors())
 
 // database config
 const connection_url = 'mongodb+srv://koonzinc:SL484W02rgEpMXkO@cluster0.rdupyvg.mongodb.net/?retryWrites=true&w=majority'
